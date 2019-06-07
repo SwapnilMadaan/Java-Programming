a7ecd8322b0a07dd280ad334eeaccb66bef88e4e
Swapnil Madaan <31646351+Swapnil97@users.noreply.github.com>
2019-06-08 01:26:53 +0530

    using extra space of hashmap

---
+++ Hashing/Array Contains Duplicate/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i:nums)
+            map.put(i,map.getOrDefault(i,0) +1);
+        for(int i:nums)
+        {
+            if(map.get(i)>1)
+                return true; 
+        }
+        return false;
+    }
+}
